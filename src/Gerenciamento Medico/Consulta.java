7eb3b4f60f22c0e587e16237aafdbbf656308357
ivogfonseca <ivinhospo@yahoo.com.br>
2024-09-25 14:57:55 -0300

    metodos

--- src/Gerenciamento Medico/Consulta.java
+++ src/Gerenciamento Medico/Consulta.java
@@ -1,3 +1,38 @@
+import java.time.LocalDateTime;
+
 public class Consulta {
+    private String nomePaciente;
+    private LocalDateTime diaConsulta;
+    private String nomeMedico;
     
+    public Consulta(String nome, LocalDateTime diaConsulta, String nomeMedico) {
+        this.nomePaciente = nome;
+        this.diaConsulta = diaConsulta;
+        this.nomeMedico = nomeMedico;
+    }
+    public String getNome() {
+        return nomePaciente;
+    }
+    public void setNome(String nome) {
+        this.nomePaciente = nome;
+    }
+    public LocalDateTime getDiaConsulta() {
+        return diaConsulta;
+    }
+    public void setDiaConsulta(LocalDateTime diaConsulta) {
+        this.diaConsulta = diaConsulta;
+    }
+    public String getNomeMedico() {
+        return nomeMedico;
+    }
+    public void setNomeMedico(String nomeMedico) {
+        this.nomeMedico = nomeMedico;
+    }
+
+    public void agendarConsulta(String nomeMedico, String nomePaciente, LocalDateTime dataHoraConsulta) {
+        this.nomeMedico = nomeMedico;
+        this.nomePaciente = nomePaciente;
+        this.diaConsulta = dataHoraConsulta;
+        System.out.println("Consulta agendada com sucesso!");
+    }
 }
