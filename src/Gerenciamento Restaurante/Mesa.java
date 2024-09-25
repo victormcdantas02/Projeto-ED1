343f9be14b6892eb3f746554c19f0b843c79f72c
Victor <victormcdantas@gmail.com>
2024-09-25 15:04:09 -0300

    Restaurante atualização

--- src/Gerenciamento Restaurante/Mesa.java
+++ src/Gerenciamento Restaurante/Mesa.java
@@ -1,3 +1,24 @@
 public class Mesa {
+    private String numero;
+    private boolean disponibilidade;
+
+    public Mesa(String numero, boolean disponibilidade) {
+        this.numero = numero;
+        this.disponibilidade = disponibilidade;
+    }
     
+    public String getNumero() {
+        return numero;
+    }
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+    public boolean isDisponibilidade() {
+        return disponibilidade;
+    }
+    public void setDisponibilidade(boolean disponibilidade) {
+        this.disponibilidade = disponibilidade;
+    }
+    
+  
 }
