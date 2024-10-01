47f06aed4211af95b6a3f93548b419f23abc5efe
GustavoLSS2004 <gustavolssilva2004@gmail.com>
2024-10-01 17:35:13 -0300

    Re-adicionados métodos excluídos acidentalmente de Evento.

--- src/Gerenciamento Evento/Evento.java
+++ src/Gerenciamento Evento/Evento.java
@@ -1,15 +1,19 @@
 import java.util.Date;
 public class Evento {
     private String nome;
+    private Date diaDoEvento;
     private String local;
     private int capacidade;
-    private Date diaDoEvento;
+    private ListaSimples<Participante> participantesEvento;
+    private ListaSimples<Evento> eventos = new ListaSimples<>();
+    
     
     public Evento(String nome, String local, int capacidade, Date diaDoEvento) {
         this.nome = nome;
         this.local = local;
         this.capacidade = capacidade;
         this.diaDoEvento = diaDoEvento;
+        participantesEvento = new ListaSimples<>();
     }
 
     public String getNome() {
@@ -42,6 +46,46 @@ public class Evento {
 
     public void setDiaDoEvento(Date diaDoEvento) {
         this.diaDoEvento = diaDoEvento;
+    }
+    
+    public ListaSimples<Participante> getParticipantesEvento() {
+        return participantesEvento;
+    }
+
+    public ListaSimples<Evento> getEventos() {
+        return eventos;
+    }
+
+    public void registrarParticipante(Participante novoParticipante){
+        if(novoParticipante == null){
+            System.out.println("Por favor, insira os dados do participante corretamente.");
+            return;
+        }
+        participantesEvento.adicionarInicio(novoParticipante);
+        System.out.println("Novo participante registrado.");
+
+    }
+
+    public void cancelarInscrição(Participante inscriçãoACancelar){
+        if(participantesEvento.estaNaLista(inscriçãoACancelar)){
+            participantesEvento.removerMeio(inscriçãoACancelar);
+            System.out.println("Inscrição cancelada.");
+        } else {
+            System.out.println("O participante não está inscrito neste evento");
+        }
+
+    }
+
+    public Evento buscarEvento(Evento eventoAlvo){
+        
+    }
+
+    public Participante buscarParticipante(Participante participante){
+
+    }
+
+    public void atualizarEvento(Evento eventoAlvo){
+
     }
     
 }
