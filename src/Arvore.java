61399d162a3120d6a420fd78be6e666cf221aad5
Victor <victormcdantas@gmail.com>
2024-11-20 23:06:06 -0300

    Correção Arvore

--- src/Arvore.java
+++ src/Arvore.java
@@ -1,24 +1,33 @@
 public class Arvore<T extends Comparable<T>> {
-    private T value;         // Valor guardado no nó
-    private Arvore<T> left;  // Filho esquerdo
-    private Arvore<T> right; // Filho direito
+    private T value;         
+    private Arvore<T> left; 
+    private Arvore<T> right; 
 
-    // Construtor
     public Arvore(T v, Arvore<T> l, Arvore<T> r) {
         value = v;
         left = l;
         right = r;
     }
 
-    // Getters e Setters
-    public T getValue() { return value; }
-    public Arvore<T> getLeft() { return left; }
-    public Arvore<T> getRight() { return right; }
-    public void setValue(T v) { value = v; }
-    public void setLeft(Arvore<T> l) { left = l; }
-    public void setRight(Arvore<T> r) { right = r; }
+    public T getValue() { 
+        return value; 
+    }
+    public Arvore<T> getLeft() { 
+        return left; 
+    }
+    public Arvore<T> getRight() { 
+        return right; 
+    }
+    public void setValue(T v) { 
+        value = v; 
+    }
+    public void setLeft(Arvore<T> l) { 
+        left = l; 
+    }
+    public void setRight(Arvore<T> r) { 
+        right = r; 
+    }
 
-    // Função para inserir um novo valor na árvore
     public void insert(T newValue) {
         if (newValue.compareTo(value) < 0) {
             if (left == null) {
@@ -35,7 +44,7 @@ public class Arvore<T extends Comparable<T>> {
         }
     }
 
-    // Função para buscar um valor na árvore
+
     public Arvore<T> search(T targetValue) {
         if (targetValue.equals(value)) {
             return this;
@@ -46,7 +55,7 @@ public class Arvore<T extends Comparable<T>> {
         }
     }
 
-    // Função para remover um valor da árvore
+
     public Arvore<T> remove(T targetValue) {
         if (targetValue.compareTo(value) < 0) {
             if (left != null) {
@@ -57,17 +66,16 @@ public class Arvore<T extends Comparable<T>> {
                 right = right.remove(targetValue);
             }
         } else {
-            // Caso 1: Nó sem filhos
+
             if (left == null && right == null) {
                 return null;
             }
-            // Caso 2: Nó com apenas um filho
+
             if (left == null) {
                 return right;
             } else if (right == null) {
                 return left;
             }
-            // Caso 3: Nó com dois filhos
             Arvore<T> minValueNode = right.findMin();
             value = minValueNode.getValue();
             right = right.remove(minValueNode.getValue());
@@ -75,7 +83,6 @@ public class Arvore<T extends Comparable<T>> {
         return this;
     }
 
-    // Função auxiliar para encontrar o menor valor em uma subárvore
     private Arvore<T> findMin() {
         return (left == null) ? this : left.findMin();
     }
